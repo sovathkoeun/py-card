1922cb5e2ba2b0492180577093256138102a8db7
sovathkoeun <sovathkoeun2020@gmail.com>
2025-02-12 22:04:18 +0700

    add news

--- card.py
+++ card.py
@@ -1,13 +1,21 @@
 
 
 card_no = "09443840258344"
+double_list = []
 odd_number =  0
 Number = list(card_no)
 for (idx, val) in enumerate(Number):
     if idx % 2 !=0:
         odd_number += int(val)
     else:
-        pass
+        double_list.append(int(val)*2)
+print(double_list)
 
-print(odd_number)
+double_string = ""
+
+for x in double_list:
+    double_string += str(x)
+
+double_list = list(double_string)
+print(double_list)
         
