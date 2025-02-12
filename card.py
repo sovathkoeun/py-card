444ef375c2ed5a28b65599cf568ba2c21afc31dd
sovathkoeun <sovathkoeun2020@gmail.com>
2025-02-12 22:08:53 +0700

    A

--- card.py
+++ card.py
@@ -2,6 +2,7 @@
 
 card_no = "09443840258344"
 double_list = []
+event_num = 0
 odd_number =  0
 Number = list(card_no)
 for (idx, val) in enumerate(Number):
@@ -15,7 +16,14 @@ double_string = ""
 
 for x in double_list:
     double_string += str(x)
+    
 
-double_list = list(double_string)
-print(double_list)
+for x in double_list:
+    event_num += int(x)
+    
+net_sum = odd_number + event_num
+if net_sum % 10 == 0:
+    print("valid card")
+else:
+    print("invalid card")
         
