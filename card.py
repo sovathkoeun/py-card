b389f799065101dd23f998bda24530e88505b84d
sovathkoeun <sovathkoeun2020@gmail.com>
2025-02-12 21:51:32 +0700

    add new

--- card.py
+++ card.py
@@ -3,5 +3,11 @@
 card_no = "09443840258344"
 odd_number =  0
 Number = list(card_no)
-for x in Number:
-    print(x)
+for (idx, val) in enumerate(Number):
+    if idx % 2 !=0:
+        odd_number += int(val)
+    else:
+        pass
+
+print(odd_number)
+        
